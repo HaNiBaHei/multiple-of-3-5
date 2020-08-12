bb45e5cb798e9ea848596e64708d03f823c476a9
CHINNAPAT SIRIYAJAI <63010231@KMITL.AC.TH>
2020-08-12 13:18:33 +0700

    change to sum of multiple of 3,5

--- multiple of 3,5/Multiple of 3,5.c
+++ multiple of 3,5/Multiple of 3,5.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 
 main() {
-	int x;
+	int x, y = 0;
 	for (x = 1; x < 1000; x++) {
 		if (x % 3 == 0 || x % 5 == 0) {
-			printf("%d\n", x);
+			y += x;
 
 		}
 	}
+	printf("%d", y);
 }
